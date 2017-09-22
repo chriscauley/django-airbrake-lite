418f34ef3292f5dafa02f659df69a6bc1e024a4c
chriscauley <chris@lablackey.com>
2017-09-22 10:33:47 -0400

    temporarily shutting down google bots in airbrake

--- airbrake/views.py
+++ airbrake/views.py
@@ -8,6 +8,8 @@ import json
 def js_error(request):
   if not 'data' in request.POST:
     raise Http404()
+  if "Googlebot" in request.POST['data'].get("userAgent",""):
+    raise Http404()
   user = request.user if request.user.is_authenticated() else None
   JSError.objects.create(
     data=json.loads(request.POST['data']),
